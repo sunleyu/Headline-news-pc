15bda7bdec07160631209bd33dd46a8eae431e39
Your Name <you@example.com>
2019-10-25 12:39:30 +0800

    下载路由插件并挂载

--- src/main.js
+++ src/main.js
@@ -2,10 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-Vue.config.productionTip = false
 
+import router from './router/index'
 Vue.use(ElementUI)
-
+Vue.config.productionTip = false
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
