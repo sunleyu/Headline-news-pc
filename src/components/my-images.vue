f557c36a79dfd76b057caf76203c81d29e26a45a
Your Name <you@example.com>
2019-11-03 19:07:04 +0800

    发布文章文章封面选择布局基本完成

--- src/components/my-images.vue
+++ src/components/my-images.vue
@@ -15,13 +15,16 @@
           </div>
           <!-- 图片列表 -->
           <div class="img_list">
-            <div class="img_item" v-for="item in images" :key="item.id">
-              <img :src="item.url" alt />
+            <div
+              class="img_item"
+              v-for="item in images"
+              :key="item.id"
+              :class="{selected: selectedImageUrl === item.url}"
+                @click="selectedImage(item.url)"
+            >
+              <img :src="item.url" />
               <div class="footer" v-if="!reqParams.collect">
-                <span
-                  class="el-icon-star-off"
-                  :class="{red:item.is_collected}"
-                ></span>
+                <span class="el-icon-star-off" :class="{red:item.is_collected}"></span>
 
                 <span class="el-icon-delete"></span>
               </div>
@@ -38,19 +41,23 @@
           </div>
         </el-tab-pane>
         <el-tab-pane label="上传文件">
-            <el-upload
-          class="avatar-uploader"
-          action="http://ttapi.research.itcast.cn/mp/v1_0/user/images"
-          name="image"
-          :headers="headers"
-          :on-success="handleSuccess"
-          :show-file-list="false"
-        >
-          <img v-if="imageUrl" :src="imageUrl" class="avatar" />
-          <i v-else class="el-icon-plus avatar-uploader-icon"></i>
-        </el-upload>
+          <el-upload
+            class="avatar-uploader"
+            action="http://ttapi.research.itcast.cn/mp/v1_0/user/images"
+            name="image"
+            :headers="headers"
+            :on-success="handleSuccess"
+            :show-file-list="false"
+          >
+            <img v-if="imageUrl" :src="imageUrl" class="avatar" />
+            <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+          </el-upload>
         </el-tab-pane>
       </el-tabs>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+      </span>
     </el-dialog>
   </div>
 </template>
@@ -64,12 +71,13 @@ export default {
         collect: false,
         // src: null
         page: 1,
-        per_page: 10
+        per_page: 8
       },
       images: [],
       total: null,
       dialogVisible: false,
       imageUrl: null,
+      selectedImageUrl: null,
       headers: {
         Authorization: `Bearer ${local.getUser().token}`
       }
@@ -88,9 +96,6 @@ export default {
     },
     btn () {
       this.dialogVisible = true
-    },
-    open () {
-      this.dialogVisible = true
       this.imageUrl = null
     },
     handleSuccess (res) {
@@ -108,13 +113,26 @@ export default {
     toggleList () {
       this.reqParams.page = 1
       this.getImages()
+    },
+    selectedImage (url) {
+      this.selectedImageUrl = url
+    //   console.log(url)
+    //   console.log(this.selectedImageUrl)
     }
   }
-
 }
 </script>
 
 <style scoped lang="less">
+.my-image {
+  display: inline-block;
+  margin-right: 20px;
+}
+.dialog-footer {
+  text-align: center;
+  width: 100%;
+  display: block;
+}
 .img_list {
   padding-top: 20px;
   .img_item {
@@ -129,22 +147,15 @@ export default {
       width: 100%;
       height: 100%;
     }
-    .footer {
+      &.selected::after {
+        content: "";
       position: absolute;
+      top: 0;
       left: 0;
-      bottom: 0;
-      height: 30px;
-      line-height: 30px;
-      background: rgba(0, 0, 0, 0.3);
       width: 100%;
-      text-align: center;
-      color: #fff;
-      span {
-        margin: 0 20px;
-        &.red {
-          color: red;
-        }
-      }
+      height: 100%;
+      background: rgba(0, 0, 0, 0.3) url(../assets/selected.png) no-repeat
+        center / 50px 50px;
     }
   }
 }
